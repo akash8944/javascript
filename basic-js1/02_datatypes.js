2d68b1d578cebda029d70a867698b9a44555f82a
akash8944 <akashsoni8944@gmail.com>
2025-09-20 06:37:33 +0000

    datatypes

---
+++ basic-js1/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as a newer version
+// alert( 3 + 3) we are user nodejs, not browser
+console.log("akash")
+
+let name = "hitesh"
+let age = 18;
+let isLoggedIn=false;
+
+//number =>2 to 53;
+// BigInt
+// String => ""
+// Boolean => true/ false
+//null = stanalone value;
+// undefiend
+//symbol = unique
+
+
+//object
+console.log(typeof undefined) //undefined
+console.log(typeof null) // object
