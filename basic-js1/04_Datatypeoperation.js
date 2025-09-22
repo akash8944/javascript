9a702b299653bed273dea84f36a6324c306ba783
akash8944 <akashsoni8944@gmail.com>
2025-09-22 03:57:21 +0000

    Datatypeoperation

---
+++ basic-js1/04_Datatypeoperation.js
@@ -0,0 +1,27 @@
+let value = 3
+let negValue = -value
+// console.log(negValue) 
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "akash"
+let str2 = " akash2 "
+// let str3 = console.log( str1 + str2)  node basic-js1/04_Datatypeoperation.js
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2");
+// console.log( (3+4))
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter)
