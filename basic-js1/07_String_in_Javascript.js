3ced1a6830544e3af7cb94486cf2bbbaf44132cc
akash8944 <akashsoni8944@gmail.com>
2025-09-26 17:33:24 +0000

    strin_in_javascript

---
+++ basic-js1/07_String_in_Javascript.js
@@ -0,0 +1,26 @@
+// const name ="akash"
+// const repcount = 50
+// // console.log(name + repcount + "value") ye aaj kal old ho gya ise koi use nhi karta 
+// console.log(`my name is ${name} and my repo count is ${repcount}`)
+
+const gamName= new String('hitesh-hs')
+// console.log(gamName.__proto__)
+// console.log(gamName.toUpperCase())
+// console.log(gamName.charAt(2))
+// console.log(gamName[0])
+// console.log(gamName.length)
+// console.log(gamName.indexOf('t'))
+// let newstring = gamName.substring(0,4)
+// console.log(newstring)
+
+// const anotherString = gameName.slice(-8, 4) //swlice me - value bhi de skte he 
+// console.log(anotherString)
+
+const anothernewstring = "   akash   "
+console.log(anothernewstring)
+console.log(anothernewstring.trim())
+
+const url = "https/akash.com/akash%20soni"
+console.log(url.replace('%20', '_'))
+console.log(url.includes('sundar'))
+console.log(gamName.split('-'))
