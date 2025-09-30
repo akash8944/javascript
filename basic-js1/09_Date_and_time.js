6fab0383f0951bad770468c594bbcbe23736a09b
akash8944 <akashsoni8944@gmail.com>
2025-09-30 17:10:40 +0000

    date&time

---
+++ basic-js1/09_Date_and_time.js
@@ -0,0 +1,18 @@
+// let mydate = new Date()
+// console.log(mydate)
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toISOString())
+// console.log(mydate.toJSON())
+// console.log(mydate.toLocaleDateString())
+// console.log(typeof mydate)
+
+// let createdate = new Date(2023,0,24)
+// console.log(createdate.toDateString())
+
+// let mycreatedate = new Date(2025,8,25,5,7)
+// let mycreatedate = new Date(2025,8,25)
+// console.log(mycreatedate.toLocaleString())
+
+let mytimestamp = Date.now()
+console.log(mytimestamp)
