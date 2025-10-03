86b97150c1aa0b04812764ac5ed2654b81c30b2d
akash8944 <akashsoni8944@gmail.com>
2025-10-03 16:30:18 +0000

    array

---
+++ basic-js2/01_array.js
@@ -0,0 +1,16 @@
+// array
+
+const myarr = [0, 1, 2, 3, 4, 5, 6];
+const myheros = ["shaktiman", "akash"];
+
+const myarr2 = new Array(1, 2, 3, 4, 5, 6); 
+console.log(myarr[0]);
+
+//array methods
+
+myarr.push(8)
+myarr.push(9)
+myarr.pop()
+myarr.unshift(9)
+myarr.shift()
+console.log(myarr)
