29d073aa3023b563cb338371550aa6b653024f8e
akash8944 <akashsoni8944@gmail.com>
2025-10-04 03:27:57 +0000

    array

--- basic-js2/01_array.js
+++ basic-js2/01_array.js
@@ -1,16 +1,39 @@
 // array
 
-const myarr = [0, 1, 2, 3, 4, 5, 6];
-const myheros = ["shaktiman", "akash"];
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
 
-const myarr2 = new Array(1, 2, 3, 4, 5, 6); 
-console.log(myarr[0]);
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
 
-//array methods
+// Array methods
 
-myarr.push(8)
-myarr.push(9)
-myarr.pop()
-myarr.unshift(9)
-myarr.shift()
-console.log(myarr)
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
