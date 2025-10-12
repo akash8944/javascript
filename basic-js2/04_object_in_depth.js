be6c4dd30f9af5d6bf7e6ef0ccddf025eca1d10d
akash8944 <akashsoni8944@gmail.com>
2025-10-12 11:15:34 +0000

    object_in_depth_2.o

---
+++ basic-js2/04_object_in_depth.js
@@ -0,0 +1,50 @@
+// const tinderuser = new Object()
+
+// const { use } = require("react")
+
+const tinderuser = {}
+
+tinderuser.id = "123abc"
+tinderuser.name = "akash"
+tinderuser.isLoggedIn = false
+// console.log(tinderuser)
+
+const regularuser = {
+    email: "akash@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname: " akash",
+            lastname: "soni"
+        }
+    }
+}
+// console.log(regularuser.fullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "b"}
+
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign({},obj1,obj2)
+
+const obj3 = {...obj1,...obj2}
+
+// console.log(obj3)
+
+const user = [
+    {
+        id: 1,
+        email:"akash@gmail.com",
+    },
+    {
+        id: 1,
+        email:"akash@gmail.com",
+    },
+    {
+        id: 1,
+        email:"akash@gmail.com",
+    },
+]
+console.log(user[1].email);
+console.log(tinderuser);
+console.log(Object.values(tinderuser));
+console.log(tinderuser.hasOwnProperty('name'));
