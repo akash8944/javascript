98b6890fee6c287d540e74e923a3e460fc50e421
akash8944 <akashsoni8944@gmail.com>
2025-10-18 17:06:58 +0000

    Scope_level_and_mini_hoisting

---
+++ basic-js3/04_Scope_level_and_mini_hoisting.js
@@ -0,0 +1,37 @@
+function one(){
+const username = "hitesh"
+
+
+function two()
+{
+    const website = " you tube "
+    console.log(username);
+}
+// console.log(website)
+two()
+}
+// one()
+
+if(true)
+{
+    const username = "hitesh"
+    if(username === "hitesh")
+    {
+        const website = " youtube "
+        console.log(username + website)
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+function addone(num)
+{
+  return num + 1
+}
+console.log(addone(5))
+
+const addTwo = function(num)
+{
+  return num + 2
+}
+addtwo(6)
